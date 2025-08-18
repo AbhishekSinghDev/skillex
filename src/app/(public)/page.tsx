2c653214f7330f0c883c9e647e0d0fcc4372aa53
AbhishekSinghDev <singhabhishek7689@gmail.com>
2025-08-18 21:40:17 +0530

    add basic landing page setup

---
+++ src/app/(public)/page.tsx
@@ -0,0 +1,14 @@
+import Footer from "@/components/shared/footer";
+import Navbar from "@/components/shared/navbar";
+
+const page = () => {
+  return (
+    <>
+      <Navbar />
+      <main>page</main>
+      <Footer />
+    </>
+  );
+};
+
+export default page;
